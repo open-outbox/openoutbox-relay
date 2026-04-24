642fd517ddbd2c609aff6da6b041420908ade149
Mohsen Mahmoodi <m.mahmoodi.biz@gmail.com>
2026-04-24 21:46:35 +0300

    Checkpoint: Remove kafka script

--- benchmark/kafka-setup.sh
+++
@@ -1,6 +0,0 @@
-docker exec -it benchmark-kafka-1 /opt/kafka/bin/kafka-topics.sh \
-  --create \
-  --topic outbox.events.v1 \
-  --bootstrap-server localhost:9092 \
-  --partitions 12 \
-  --replication-factor 1
